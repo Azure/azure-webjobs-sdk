c901e4341f7a6ae6cf85317eb687bff35a691431
Mathew Charles <mathewc@microsoft.com>
2015-06-23 16:18:25 -0700

    Allowing Queues "NewBatchThreshold" to be configured both globally as well as per queue (via QueueProcessor)

---
+++ test/Microsoft.Azure.WebJobs.Host.UnitTests/JobHostQueuesConfigurationTests.cs
@@ -0,0 +1,35 @@
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the MIT License. See License.txt in the project root for license information.
+
+using Microsoft.Azure.WebJobs.Host.Queues;
+using Microsoft.Azure.WebJobs.Host.Queues.Listeners;
+using Xunit;
+
+namespace Microsoft.Azure.WebJobs.Host.UnitTests
+{
+    public class JobHostQueuesConfigurationTests
+    {
+        [Fact]
+        public void Constructor_Defaults()
+        {
+            // Arrange
+            JobHostQueuesConfiguration config = new JobHostQueuesConfiguration();
+
+            // Act & Assert
+            Assert.Equal(16, config.BatchSize);
+            Assert.Equal(8, config.NewBatchThreshold);
+            Assert.Equal(typeof(DefaultQueueProcessorFactory), config.QueueProcessorFactory.GetType());
+            Assert.Equal(QueuePollingIntervals.DefaultMaximum, config.MaxPollingInterval);
+        }
+
+        [Fact]
+        public void NewBatchThreshold_CanSetAndGetValue()
+        {
+            // Arrange
+            JobHostQueuesConfiguration config = new JobHostQueuesConfiguration();
+
+            config.NewBatchThreshold = 1000;
+            Assert.Equal(1000, config.NewBatchThreshold);
+        }
+    }
+}
