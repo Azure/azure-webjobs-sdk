4c510820f4b33e3c7bb6c2c58c62aa0e13114d9c
Mike Stall <mikestall@hotmail.com>
2016-05-13 15:29:52 -0700

    Add local validator.

--- test/Microsoft.Azure.WebJobs.Host.UnitTests/Common/ValidationTests.cs
+++ test/Microsoft.Azure.WebJobs.Host.UnitTests/Common/ValidationTests.cs
@@ -15,7 +15,6 @@ namespace Microsoft.Azure.WebJobs.Host.UnitTests.Common
 {
     public class ValidationTests
     {
-
         public class TestAttribute : Attribute
         {
             public bool Bad { get; set; }
@@ -24,10 +23,28 @@ namespace Microsoft.Azure.WebJobs.Host.UnitTests.Common
 
             [AutoResolve]
             public string Path { get; set; }
+
+
+            public void ValidateAtIndexTime(Type parameterType)
+            {
+                // Verify that FX is passing the expected paramteer type.
+                Assert.True(parameterType == typeof(Widget) || parameterType == typeof(Widget2));
+
+                // Test that validation is valled after INameResolver has handled %%, but before {} are resolved. 
+                Assert.Equal("v1-{k2}", this.Path);
+
+                if (this.Bad)
+                {
+                    throw new InvalidOperationException(TestAttribute.ErrorMessage);
+                }
+            }
         }
 
+        // Some arbitrary types to use in parameters.  
+        public class Widget { }
+
         // Some arbitrary type. 
-        public class Widget { } 
+        public class Widget2 { }
 
         public class FunctionBase
         {
@@ -60,18 +77,9 @@ namespace Microsoft.Azure.WebJobs.Host.UnitTests.Common
                 context.RegisterBindingRules<TestAttribute>(ValidateAtIndexTime, rule);
             }
 
-            public void ValidateAtIndexTime(TestAttribute attribute, Type parameterType)
+            private static void ValidateAtIndexTime(TestAttribute attribute, Type parameterType)
             {
-                // Verify that FX is passing the expected paramteer type.
-                Assert.Equal(typeof(Widget), parameterType);
-
-                // Test that validation is valled after INameResolver has handled %%, but before {} are resolved. 
-                Assert.Equal("v1-{k2}", attribute.Path);
-
-                if (attribute.Bad)
-                {
-                    throw new InvalidOperationException(TestAttribute.ErrorMessage);
-                }
+                attribute.ValidateAtIndexTime(parameterType);
             }
         }
 
@@ -81,17 +89,9 @@ namespace Microsoft.Azure.WebJobs.Host.UnitTests.Common
             var nr = new FakeNameResolver().Add("k1", "v1");
             var host = TestHelpers.NewJobHost<BadFunction>(new FakeExtClient(), nr);
 
-            try
-            {
-                host.Call("Valid"); // Should get indexing error from 'Bad' function 
-            }
-            catch (FunctionIndexingException e)
-            {
-                Assert.Equal("Error indexing method 'BadFunction.Bad'", e.Message);
-                Assert.Equal(TestAttribute.ErrorMessage, e.InnerException.Message);
-                return;
-            }
-            Assert.True(false, "Invoker should have failed");
+            TestHelpers.AssertIndexingError(
+                () => host.Call("Valid"),
+                "BadFunction.Bad", TestAttribute.ErrorMessage);
         }
 
         [Fact]
@@ -100,6 +100,68 @@ namespace Microsoft.Azure.WebJobs.Host.UnitTests.Common
             var nr = new FakeNameResolver().Add("k1", "v1");
             var host = TestHelpers.NewJobHost<GoodFunction>(new FakeExtClient(), nr);
             host.Call("Good", new { k2 = "xxxx" } ); 
+        }
+
+        // Register [Test]  with 2 rules and a local validator. 
+        public class FakeExtClient2 : IExtensionConfigProvider
+        {
+            public void Initialize(ExtensionConfigContext context)
+            {
+                var bf = context.Config.BindingFactory;
+
+                // Add [Test] support
+                var rule1 = bf.BindToExactType<TestAttribute, Widget>(attr => new Widget());
+                var rule2 = bf.BindToExactType<TestAttribute, Widget2>(attr => new Widget2());
+                var rule2Validator = bf.AddValidator<TestAttribute>(LocalValidator, rule2);
+
+                context.RegisterBindingRules<TestAttribute>(rule2Validator, rule1);
+            }
+
+            private Task LocalValidator(TestAttribute attribute, Type parameterType)
+            {
+                attribute.ValidateAtIndexTime(parameterType);
+                return Task.FromResult(0);
+            }
+        }
+
+        public class LocalFunction1 : FunctionBase
+        {
+            // No validation on Widget-rule to cathc that ths is bad. 
+            // So this passes. Validator is never run. 
+            public void NoValidation([Test(Bad = true, Path = "%k1%-{k2}")] Widget x)
+            {
+            }
+        }
+
+        public class LocalFunction2 : FunctionBase
+        {
+            // Validation rule does run on this parameter, catches the failure. 
+            public void WithValidation([Test(Bad = true, Path = "%k1%-{k2}")] Widget2 x)
+            {
+            }
+        }
+
+        [Fact]
+        public void TestLocalValidatorSkipped()
+        {
+            // Local validator only run if we use the given rule. 
+            var nr = new FakeNameResolver().Add("k1", "v1");
+            var host = TestHelpers.NewJobHost<LocalFunction1>(new FakeExtClient2(), nr);
+
+            host.Call("NoValidation", new { k2 = "xxxx" }); // Succeeds since validate doesn't run on this rule             
+        }
+
+        [Fact]
+        public void TestLocalValidatorApplied()
+        {
+            // Local validator only run if we use the given rule. 
+            var nr = new FakeNameResolver().Add("k1", "v1");
+            var host = TestHelpers.NewJobHost<LocalFunction2>(new FakeExtClient2(), nr);
+
+            TestHelpers.AssertIndexingError(
+                () => host.Call("WithValidation"),
+                "LocalFunction2.WithValidation", TestAttribute.ErrorMessage);
+            
         }
     }
 }
