ec5b13248806f650489a934f8ecb2a139128e204
Mike Stall <mikestall@hotmail.com>
2016-10-10 11:37:54 -0700

    Add unit tests for Dashboard. Test registration, No functional changes to the product.
    Switch over the DI from AutoFac to Ninject.
    - Ninject uses global statics and is harder to test.
    - the cool kids recommend AutoFac.

---
+++ test/Dashboard.UnitTests/AutoFacTests.cs
@@ -0,0 +1,44 @@
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the MIT License. See License.txt in the project root for license information.
+
+using System;
+using System.Configuration;
+using Autofac;
+using Dashboard.Data;
+using Xunit;
+
+
+namespace Dashboard.UnitTests.Data
+{
+    public class AutoFacTests
+    {
+        private const string FunctionLogTableAppSettingName = "AzureWebJobsLogTableName";
+
+        [Theory]
+        [InlineData(FunctionLogTableAppSettingName, "logTestTable123")] // USe fast logging 
+        [InlineData(FunctionLogTableAppSettingName, null)] // use classic SDK logging
+        public void RegistrationTest(string appsetting, string value)
+        {
+            var oldSetting = ConfigurationManager.AppSettings[appsetting];
+            try
+            {
+                ConfigurationManager.AppSettings[appsetting] = value;
+
+                var container = MvcApplication.BuildContainer();
+
+                // Verify we can create all the API & MVC controller classes. 
+                // This is really testing that all dependencies are properly registered 
+                container.Resolve<ApiControllers.DiagnosticsController>();
+                container.Resolve<ApiControllers.FunctionsController>();
+                container.Resolve<ApiControllers.LogController>();
+
+                container.Resolve<Controllers.FunctionController>();
+                container.Resolve<Controllers.MainController>();
+            }
+            finally
+            {
+                ConfigurationManager.AppSettings[appsetting] = oldSetting;
+            }
+        }
+    }
+}
