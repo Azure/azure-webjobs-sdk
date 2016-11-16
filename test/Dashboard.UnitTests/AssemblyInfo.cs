bf297d79710fb608ba2dd400b30be59a89b7587d
Matt Mason <mamaso@microsoft.com>
2016-11-16 10:10:50 -0800

    Add appveyor test support

---
+++ test/Dashboard.UnitTests/AssemblyInfo.cs
@@ -0,0 +1,4 @@
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the MIT License. See License.txt in the project root for license information.
+using Xunit;
+[assembly: TestFramework("Microsoft.Azure.WebJobs.Host.TestCommon.SyncContextTestFramework", "Microsoft.Azure.WebJobs.Host.TestCommon")]
