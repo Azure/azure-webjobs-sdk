6a57f51f1d313822d16791688fb58ed87a5e71c9
Hamza Hawkins <hhawkins@users.noreply.github.com>
2017-05-30 17:13:42 -0700

    Prototype of the Invocation Filter Attribute

---
+++ src/Microsoft.Azure.WebJobs/FunctionExecutedContext.cs
@@ -0,0 +1,11 @@
+﻿// FunctionExecutedContext.cs
+
+namespace Microsoft.Azure.WebJobs
+{
+    /// <summary>
+    /// The context for an executed function. This needs to be expanded on later.
+    /// </summary>
+    public class FunctionExecutedContext
+    {
+    }
+}
