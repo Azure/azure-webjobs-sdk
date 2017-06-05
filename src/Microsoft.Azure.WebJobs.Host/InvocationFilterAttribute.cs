997449b2097d898055008ec1b90f79b4e1a5200b
Hamza Hawkins <hhawkins@users.noreply.github.com>
2017-06-05 09:58:02 -0700

    Logging Filter Test and Other Changes

--- src/Microsoft.Azure.WebJobs.Host/InvocationFilterAttribute.cs
+++ src/Microsoft.Azure.WebJobs.Host/InvocationFilterAttribute.cs
@@ -9,16 +9,17 @@ namespace Microsoft.Azure.WebJobs
     /// <summary>
     /// This is the first iteration for the Invocation Filter Attribute
     /// </summary>
+    [CLSCompliant(false)]
     [AttributeUsage(AttributeTargets.Class | AttributeTargets.Method, Inherited = true, AllowMultiple = false)]
     public abstract class InvocationFilterAttribute : Attribute
     {
         /// <summary>
         /// Tasks here should execute before the actual function is executed
         /// </summary>
-        /// <param name="actionContext"></param>
+        /// <param name="executingContext"></param>
         /// <param name="cancellationToken"></param>
         /// <returns></returns>
-        public virtual Task OnExecutingAsync(Object actionContext, CancellationToken cancellationToken)
+        public virtual Task OnExecutingAsync(FunctionExecutingContext executingContext, CancellationToken cancellationToken)
         {
             return Task.CompletedTask;
         }
@@ -26,10 +27,10 @@ namespace Microsoft.Azure.WebJobs
         /// <summary>
         /// Tasks here should execute after the actual function is executed
         /// </summary>
-        /// <param name="actionExecutedContext"></param>
+        /// <param name="executedContext"></param>
         /// <param name="cancellationToken"></param>
         /// <returns></returns>
-        public virtual Task OnActionExecuted(Object actionExecutedContext, CancellationToken cancellationToken)
+        public virtual Task OnActionExecuted(FunctionExecutedContext executedContext, CancellationToken cancellationToken)
         {
             return Task.CompletedTask;
         }
