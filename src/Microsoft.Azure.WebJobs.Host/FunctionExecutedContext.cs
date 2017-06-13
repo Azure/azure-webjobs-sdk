90b7833e2c2354b42bb8278911ae55b808f15d20
Hamza Hawkins <hhawkins@users.noreply.github.com>
2017-06-13 11:50:43 -0700

    Finished tests, started working on invoke function filter

--- src/Microsoft.Azure.WebJobs.Host/FunctionExecutedContext.cs
+++ src/Microsoft.Azure.WebJobs.Host/FunctionExecutedContext.cs
@@ -1,15 +1,17 @@
 ﻿// FunctionExecutedContext.cs
 
 using System;
+using System.Collections.Generic;
+using Microsoft.Azure.WebJobs.Host.Executors;
 using Microsoft.Extensions.Logging;
 
-namespace Microsoft.Azure.WebJobs
+namespace Microsoft.Azure.WebJobs.Host
 {
     /// <summary>
     /// The context for an executed function. This needs to be expanded on later.
     /// </summary>
     [CLSCompliant(false)]
-    public class FunctionExecutedContext
+    public class FunctionExecutedContext : FunctionInvocationContext
     {
         /// <summary>
         /// Constructor to set the context
@@ -18,28 +20,16 @@ namespace Microsoft.Azure.WebJobs
         /// <param name="fullName"></param>
         /// <param name="arguments"></param>
         /// <param name="logger"></param>
-        public FunctionExecutedContext(Guid id, string fullName, object[] arguments, ILogger logger)
+        /// <param name="result"></param>
+        public FunctionExecutedContext(Guid id, string fullName, object[] arguments, ILogger logger, FunctionResult result) :
+            base(id, fullName, arguments, logger)
         {
-            Id = id;
-            FullName = fullName;
-            Arguments = arguments;
-            Logger = logger;
+            Result = result;
         }
 
-        /// <summary>Gets or sets the ID of the function.</summary>
-        public Guid Id { get; set; }
-
-        /// <summary>Gets or sets the fully qualified name of the function.</summary>
-        public string FullName { get; set; }
-
         /// <summary>
-        /// Gets or sets the parameters of the function
+        /// Make sure the final result is logged
         /// </summary>
-        public object[] Arguments { get; set; }
-        
-        /// <summary>
-        /// Gets or sets the logger of the function
-        /// </summary>
-        public ILogger Logger { get; set; }
+        public FunctionResult Result { get; set; }
     }
 }
---
+++ src/Microsoft.Azure.WebJobs.Host/InvokeFunctionFilterAttribute.cs
@@ -0,0 +1,54 @@
+﻿// InvokeFunctionAttribute.cs
+
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
+
+namespace Microsoft.Azure.WebJobs.Host
+{
+    /// <summary>
+    /// This is the first iteration of the InvokeFunctionAttribute
+    /// </summary>
+    [CLSCompliant(false)]
+    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Method, Inherited = true, AllowMultiple = false)]
+    public sealed class InvokeFunctionFilterAttribute : InvocationFilterAttribute
+    {
+        /// <summary>
+        /// When this attribute is used, take the name of the function to invoke
+        /// </summary>
+        /// <param name="functionName"></param>
+        public InvokeFunctionFilterAttribute(string functionName)
+        {
+            FunctionName = functionName;
+        }
+
+        /// <summary>
+        /// This is the function name to invoke
+        /// </summary>
+        public string FunctionName { get; }
+
+        /// <summary>
+        /// Call the requested function
+        /// </summary>
+        /// <returns></returns>
+        public override Task OnExecutingAsync(FunctionExecutingContext executingContext, CancellationToken cancellationToken)
+        {
+            if (executingContext == null)
+            {
+                throw new ArgumentNullException("executingContext");
+            }
+
+            executingContext.Logger.LogInformation("Executing function: " + FunctionName);
+            executingContext.Logger.LogInformation(executingContext.GetArguments().ToString());
+
+            // TODO: Implement invoke function logic
+
+            // object[] paramaters = executingContext.GetArguments();
+            // JobHost host = executingContext.Host;
+            // await host.CallAsync(FunctionName, cancellationToken);
+
+            return Task.CompletedTask;
+        }
+    }
+}
