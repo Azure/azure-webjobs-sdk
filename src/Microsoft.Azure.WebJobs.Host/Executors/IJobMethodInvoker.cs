41b4413e567a5c5d745b77444d71d2fab4aa8546
Hamza Hawkins <hhawkins@users.noreply.github.com>
2017-06-22 17:40:55 -0700

    Adding the interface for JobHost calls

---
+++ src/Microsoft.Azure.WebJobs.Host/Executors/IJobMethodInvoker.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Microsoft.Azure.WebJobs.Host.Executors
+{
+    internal interface IJobMethodInvoker
+    {
+        Task JobInvokeAsync(string method, IDictionary<string, object> parameters, CancellationToken cancellationToken);
+    }
+}
