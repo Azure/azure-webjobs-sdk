9ccb26f7ab2f5173d229b8c875387ea5e5db4363
ShunXian Cai <shucai@microsoft.com>
2017-08-23 17:22:13 -0700

    test Create() function

--- test/Microsoft.Azure.WebJobs.Host.UnitTests/Blobs/BlobPathTests.cs
+++ test/Microsoft.Azure.WebJobs.Host.UnitTests/Blobs/BlobPathTests.cs
@@ -8,8 +8,23 @@ using Xunit.Extensions;
 
 namespace Microsoft.Azure.WebJobs.Host.UnitTests.Blobs
 {
-    public class BlobPathTests 
+    public class BlobPathTests
     {
+        [Fact]
+        public void CreateBlobWithValidPattern()
+        {
+            // one parameter
+            Assert.Equal("{url}", BindableBlobPath.Create("{url}").ToString());
+            // one parameter with dot operator
+            Assert.Equal("{a.b.c}", BindableBlobPath.Create("{a.b.c}").ToString());
+            // full blob url
+            Assert.Equal("archivecontainershun/canaryeh/test/0/2017/08/01/00/09/27.avro",
+                BindableBlobPath.Create("https://shunsouthcentralus.blob.core.windows.net/archivecontainershun/canaryeh/test/0/2017/08/01/00/09/27.avro").ToString());
+            // invalid partial blob url string
+            ExceptionAssert.ThrowsFormat(() => BindableBlobPath.Create("shunsouthcentralus.blob.core.windows.net/container/blob"), "Invalid container name: shunsouthcentralus.blob.core.windows.net");
+            // more than one parameter
+            ExceptionAssert.ThrowsFormat(() => BindableBlobPath.Create("{hostName}+{Container}+{Blob}"), "Invalid blob path '{hostName}+{Container}+{Blob}'. Paths must be in the format 'container/blob' or 'blob Url'.");
+        }
         [Fact]
         public void ParseAndValidate_IfInvalidContainerName_ThrowsFormatException()
         {
