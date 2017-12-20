0c67583e0027a2dd3bf95cba257da3e5f3212149
Pragna Gopa <pgopa@microsoft.com>
2017-12-20 14:18:54 -0800

    Fix Sanitizer (#1477)
    * Fix Sanitizer #1477

--- test/Microsoft.Azure.WebJobs.Host.UnitTests/Loggers/WebJobsSanitizingInitializerTests.cs
+++ test/Microsoft.Azure.WebJobs.Host.UnitTests/Loggers/WebJobsSanitizingInitializerTests.cs
@@ -11,6 +11,11 @@ namespace Microsoft.Azure.WebJobs.Host.UnitTests.Loggers
     {
         private const string StorageString = "DefaultEndpointsProtocol=https;AccountName=testaccount;AccountKey=testkey;";
         private const string SecretReplacement = "[Hidden Credential]";
+        private const string AssemblyLoadErrorWithAllowedToken = "System.AggregateException: aggregate error --->System.IO.FileNotFoundException: Could not load file or assembly 'Microsoft.Azure.WebJobs.Host, Version=2.0.0.0, Culture=neutral, PublicKeyToken=31bf3856ad364e35' or one of its dependencies.The system cannot find the file specified.at System.Reflection.RuntimeAssembly._nLoad(AssemblyName fileName, String codeBase, Evidence assemblySecurity, RuntimeAssembly locationHint, StackCrawlMark & stackMark, IntPtr pPrivHostBinder, Boolean throwOnFileNotFound, Boolean forIntrospection, Boolean suppressSecurityChecks)";
+        private const string AssemblyLoadError = "System.AggregateException: aggregate error --->System.IO.FileNotFoundException: Could not load file or assembly 'Microsoft.Azure.WebJobs.Host, Version=2.0.0.0, Culture=neutral, Token=31bf3856ad364e35' or one of its dependencies.The system cannot find the file specified.at System.Reflection.RuntimeAssembly._nLoad(AssemblyName fileName, String codeBase, Evidence assemblySecurity, RuntimeAssembly locationHint, StackCrawlMark & stackMark, IntPtr pPrivHostBinder, Boolean throwOnFileNotFound, Boolean forIntrospection, Boolean suppressSecurityChecks)";
+        private const string AssemblyLoadErrorSanitized = "System.AggregateException: aggregate error --->System.IO.FileNotFoundException: Could not load file or assembly 'Microsoft.Azure.WebJobs.Host, Version=2.0.0.0, Culture=neutral, [Hidden Credential]' or one of its dependencies.The system cannot find the file specified.at System.Reflection.RuntimeAssembly._nLoad(AssemblyName fileName, String codeBase, Evidence assemblySecurity, RuntimeAssembly locationHint, StackCrawlMark & stackMark, IntPtr pPrivHostBinder, Boolean throwOnFileNotFound, Boolean forIntrospection, Boolean suppressSecurityChecks)";
+        private const string TestStrigWithAllowedTokenAndSecretToken = "Test String \"PublicKeyToken=31bf3856ad364e35 Token=31bf3856ad364e35\"";
+        private const string TestStrigWithAllowedTokenAndSecretTokenSanitized = "Test String \"PublicKeyToken=31bf3856ad364e35 [Hidden Credential]\"";
 
         [Fact]
         public void Sanitizes_Properties()
@@ -24,6 +29,9 @@ namespace Microsoft.Azure.WebJobs.Host.UnitTests.Loggers
             telemetry.Properties.Add("2", $"Nested \"{StorageString}\" String");
             telemetry.Properties.Add("3", $"Multiple Nested \"{StorageString}\" Strings \"{StorageString}\"");
             telemetry.Properties.Add("4", $"Nested {StorageString} String");
+            telemetry.Properties.Add("5", AssemblyLoadErrorWithAllowedToken);
+            telemetry.Properties.Add("6", AssemblyLoadError);
+            telemetry.Properties.Add("7", TestStrigWithAllowedTokenAndSecretToken);
 
             initializer.Initialize(telemetry);
 
@@ -33,6 +41,12 @@ namespace Microsoft.Azure.WebJobs.Host.UnitTests.Loggers
 
             // there's no terminator so the rest of the string is stripped
             Assert.Equal(telemetry.Properties["4"], $"Nested {SecretReplacement}");
+
+            // No secrets in the string. Keep the original
+            Assert.Equal(telemetry.Properties["5"], AssemblyLoadErrorWithAllowedToken);
+
+            Assert.Equal(telemetry.Properties["6"], AssemblyLoadErrorSanitized);
+            Assert.Equal(telemetry.Properties["7"], TestStrigWithAllowedTokenAndSecretTokenSanitized);
         }
 
         [Fact]
