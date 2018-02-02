852e58d54dcb73ef9c8cf1b634dc1103770932b8
Fabio Cavalcante <fabio.cavalcante@microsoft.com>
2018-02-02 13:20:27 -0800

    Skipping packaging and exiting with proper code on build failure

--- Build.ps1
+++ Build.ps1
@@ -1,1 +1,21 @@
-﻿dotnet build -v q
+﻿param (
+  [string]$buildNumber = "0"
+)
+
+dotnet --version
+
+dotnet build Webjobs.sln -v q
+
+if (-not $?) { exit 1 }
+
+dotnet pack src\Microsoft.Azure.WebJobs\WebJobs.csproj -o ..\..\buildoutput --no-build --version-suffix $buildNumber
+
+dotnet pack src\Microsoft.Azure.WebJobs.Host\WebJobs.Host.csproj -o ..\..\buildoutput --no-build --version-suffix $buildNumber
+
+dotnet pack src\Microsoft.Azure.WebJobs.Logging\WebJobs.Logging.csproj -o ..\..\buildoutput --no-build --version-suffix $buildNumber
+
+dotnet pack src\Microsoft.Azure.WebJobs.Logging.ApplicationInsights\WebJobs.Logging.ApplicationInsights.csproj -o ..\..\buildoutput --no-build --version-suffix $buildNumber
+
+dotnet pack src\Microsoft.Azure.WebJobs.ServiceBus\EventHubs\WebJobs.EventHubs.csproj -o ..\..\..\buildoutput --no-build --version-suffix $buildNumber
+
+dotnet pack src\Microsoft.Azure.WebJobs.ServiceBus\WebJobs.ServiceBus.csproj -o ..\..\buildoutput --no-build --version-suffix $buildNumber
