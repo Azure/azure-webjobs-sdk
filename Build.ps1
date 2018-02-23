f198a2ddff213251a52569516f27563558cb67e7
Brett Samblanet <brettsam@microsoft.com>
2018-02-23 13:20:41 -0800

    fixing nuget package naming

--- Build.ps1
+++ Build.ps1
@@ -8,14 +8,14 @@ dotnet build Webjobs.sln -v q
 
 if (-not $?) { exit 1 }
 
-dotnet pack src\Microsoft.Azure.WebJobs\WebJobs.csproj -o ..\..\buildoutput --no-build --version-suffix $buildNumber
+dotnet pack src\Microsoft.Azure.WebJobs\WebJobs.csproj -o ..\..\buildoutput --no-build --version-suffix -$buildNumber
 
-dotnet pack src\Microsoft.Azure.WebJobs.Host\WebJobs.Host.csproj -o ..\..\buildoutput --no-build --version-suffix $buildNumber
+dotnet pack src\Microsoft.Azure.WebJobs.Host\WebJobs.Host.csproj -o ..\..\buildoutput --no-build --version-suffix -$buildNumber
 
-dotnet pack src\Microsoft.Azure.WebJobs.Logging\WebJobs.Logging.csproj -o ..\..\buildoutput --no-build --version-suffix $buildNumber
+dotnet pack src\Microsoft.Azure.WebJobs.Logging\WebJobs.Logging.csproj -o ..\..\buildoutput --no-build --version-suffix -$buildNumber
 
-dotnet pack src\Microsoft.Azure.WebJobs.Logging.ApplicationInsights\WebJobs.Logging.ApplicationInsights.csproj -o ..\..\buildoutput --no-build --version-suffix $buildNumber
+dotnet pack src\Microsoft.Azure.WebJobs.Logging.ApplicationInsights\WebJobs.Logging.ApplicationInsights.csproj -o ..\..\buildoutput --no-build --version-suffix -$buildNumber
 
-dotnet pack src\Microsoft.Azure.WebJobs.ServiceBus\EventHubs\WebJobs.EventHubs.csproj -o ..\..\..\buildoutput --no-build --version-suffix $buildNumber
+dotnet pack src\Microsoft.Azure.WebJobs.ServiceBus\EventHubs\WebJobs.EventHubs.csproj -o ..\..\..\buildoutput --no-build --version-suffix -$buildNumber
 
-dotnet pack src\Microsoft.Azure.WebJobs.ServiceBus\WebJobs.ServiceBus.csproj -o ..\..\buildoutput --no-build --version-suffix $buildNumber
+dotnet pack src\Microsoft.Azure.WebJobs.ServiceBus\WebJobs.ServiceBus.csproj -o ..\..\buildoutput --no-build --version-suffix -$buildNumber
