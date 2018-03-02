edc008dff140b51de1734e9ac53949accb360e98
Brett Samblanet <brettsam@microsoft.com>
2018-03-02 13:48:32 -0800

    build improvements

--- src/Common/CommonAssemblyInfo.cs
+++ src/Common/CommonAssemblyInfo.cs
@@ -1,13 +1,21 @@
-﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Copyright (c) .NET Foundation. All rights reserved.
 // Licensed under the MIT License. See License.txt in the project root for license information.
 
-using System;
 using System.Reflection;
 using System.Runtime.InteropServices;
 
 [assembly: AssemblyProduct("Microsoft Azure WebJobs SDK")]
 [assembly: AssemblyCompany("Microsoft Corporation")]
 [assembly: AssemblyCopyright("© Microsoft Corporation. All rights reserved.")]
+
+[assembly: AssemblyVersion("2.2.0.0")]
+
+// The official build will replace the third place with the build number.
+// For example, 1.0.0.0 becomes 1.0.1234.0
+[assembly: AssemblyFileVersion("2.2.0.0")]
+
+// The official build will insert the commit hash here.
+[assembly: AssemblyInformationalVersion("")]
 
 [assembly: ComVisible(false)]
 
