2b7cc8f4e5f654eb613cfde396f05749bbd5844d
Brett Samblanet <brettsam@microsoft.com>
2018-03-21 14:42:57 -0700

    moving EventHubs folder; updating build to handle tags in AppVeyor

--- Build.ps1
+++ Build.ps1
@@ -8,14 +8,22 @@ dotnet build Webjobs.sln -v q
 
 if (-not $?) { exit 1 }
 
-dotnet pack src\Microsoft.Azure.WebJobs\WebJobs.csproj -o ..\..\buildoutput --no-build --version-suffix -$buildNumber
+$projects = 
+  "src\Microsoft.Azure.WebJobs\WebJobs.csproj",
+  "src\Microsoft.Azure.WebJobs.Host\WebJobs.Host.csproj",
+  "src\Microsoft.Azure.WebJobs.Logging\WebJobs.Logging.csproj",
+  "src\Microsoft.Azure.WebJobs.Logging.ApplicationInsights\WebJobs.Logging.ApplicationInsights.csproj",
+  "src\Microsoft.Azure.WebJobs.Extensions.EventHubs\WebJobs.EventHubs.csproj",
+  "src\Microsoft.Azure.WebJobs.ServiceBus\WebJobs.ServiceBus.csproj"
 
-dotnet pack src\Microsoft.Azure.WebJobs.Host\WebJobs.Host.csproj -o ..\..\buildoutput --no-build --version-suffix -$buildNumber
-
-dotnet pack src\Microsoft.Azure.WebJobs.Logging\WebJobs.Logging.csproj -o ..\..\buildoutput --no-build --version-suffix -$buildNumber
-
-dotnet pack src\Microsoft.Azure.WebJobs.Logging.ApplicationInsights\WebJobs.Logging.ApplicationInsights.csproj -o ..\..\buildoutput --no-build --version-suffix -$buildNumber
-
-dotnet pack src\Microsoft.Azure.WebJobs.ServiceBus\EventHubs\WebJobs.EventHubs.csproj -o ..\..\..\buildoutput --no-build --version-suffix -$buildNumber
-
-dotnet pack src\Microsoft.Azure.WebJobs.ServiceBus\WebJobs.ServiceBus.csproj -o ..\..\buildoutput --no-build --version-suffix -$buildNumber
+foreach ($project in $projects)
+{
+  $cmd = "pack", "$project", "-o", "..\..\buildoutput", "--no-build"
+  
+  if ($buildNumber -ne "0")
+  {
+    $cmd += "--version-suffix", "-$buildNumber"
+  }
+  
+  & dotnet $cmd  
+}
