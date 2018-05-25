6273b8ec6cdc27ea11877a03720a0774f3ce6010
Connor McMahon <connorm333@gmail.com>
2018-05-25 14:12:01 -0700

    Add support for extensions that consume websockets

---
+++ src/Microsoft.Azure.WebJobs.Host/Config/DelayedFunctionExecution.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Azure.WebJobs.Host.Executors;
+
+namespace Microsoft.Azure.WebJobs.Host.Config
+{
+    public class DelayedFunctionExecution
+    {
+        private readonly ITriggeredFunctionExecutor _executor;
+        private readonly TriggeredFunctionData _data;
+
+        public DelayedFunctionExecution(ITriggeredFunctionExecutor executor, TriggeredFunctionData data)
+        {
+            _executor = executor;
+            _data = data;
+        }
+
+        public async Task<FunctionResult> Execute(CancellationToken token)
+        {
+            return await _executor.TryExecuteAsync(_data, token);
+        }
+    }
+}
---
+++ src/Microsoft.Azure.WebJobs.Host/Config/IWebSocketConsumer.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.WebSockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Microsoft.Azure.WebJobs.Host.Config
+{
+    public interface IWebSocketConsumer
+    {
+        Task<DelayedFunctionExecution> GetFunctionExecutionAsync(WebSocket socket, CancellationToken token);
+    }
+}
