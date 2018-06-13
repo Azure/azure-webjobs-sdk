3a4d25ef5860ae5c954bcb624a2cf7ec23e39dee
Fabio Cavalcante <fabio.cavalcante@microsoft.com>
2018-06-13 15:06:53 -0700

    Initial startup registration support

--- src/Microsoft.Azure.WebJobs.Host/Hosting/WebJobsHostExtensions.cs
+++ src/Microsoft.Azure.WebJobs.Host/Hosting/WebJobsHostExtensions.cs
@@ -59,5 +59,55 @@ namespace Microsoft.Extensions.Hosting
 
             return builder;
         }
+
+        public static IHostBuilder UseWebJobsStartup<T>(this IHostBuilder builder) where T : IWebJobsStartup, new()
+        {
+            return builder.UseWebJobsStartup(typeof(T));
+        }
+
+        public static IHostBuilder UseWebJobsStartup(this IHostBuilder builder, Type startupType)
+        {
+            if (!typeof(IWebJobsStartup).IsAssignableFrom(startupType))
+            {
+                throw new ArgumentException($"The {nameof(startupType)} argument must be an implementation of {typeof(IWebJobsStartup).FullName}");
+            }
+
+            IWebJobsStartup startup = (IWebJobsStartup)Activator.CreateInstance(startupType);
+            startup.Configure(builder);
+
+            return builder;
+        }
+
+        /// <summary>
+        /// Enables use of external configuration providers, allowing them to inject services and update
+        /// configuration during the host initialization process.
+        /// Type discovery is performed using the <see cref="DefaultStartupTypeDiscoverer"/>.
+        /// </summary>
+        /// <param name="builder">The <see cref="IHostBuilder"/> instance to configure.</param>
+        /// <returns>The updated <see cref="IHostBuilder"/> instance.</returns>
+        public static IHostBuilder UseExternalStartup(this IHostBuilder builder)
+        {
+            return builder.UseExternalStartup(new DefaultStartupTypeDiscoverer());
+        }
+
+        /// <summary>
+        /// Enables use of external configuration providers, allowing them to inject services and update
+        /// configuration during the host initialization process.
+        /// </summary>
+        /// <param name="builder">The <see cref="IHostBuilder"/> instance to configure.</param>
+        /// <param name="typeDiscoverer">An implementation of <see cref="IWebJobsStartupTypeDiscoverer"/> that provides a list of types that 
+        /// should be used in the startup process.</param>
+        /// <returns>The updated <see cref="IHostBuilder"/> instance.</returns>
+        public static IHostBuilder UseExternalStartup(this IHostBuilder builder, IWebJobsStartupTypeDiscoverer typeDiscoverer)
+        {
+            Type[] types = typeDiscoverer.GetStartupTypes();
+
+            foreach (var type in types)
+            {
+                builder.UseWebJobsStartup(type);
+            }
+
+            return builder;
+        }
     }
 }
