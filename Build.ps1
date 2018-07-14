94ef7b62a80204010a606d3e1a6589db38cebf17
Fabio Cavalcante <fabio.cavalcante@microsoft.com>
2018-07-13 17:56:02 -0700

    Updating build file to ease local package publishing

--- Build.ps1
+++ Build.ps1
@@ -1,7 +1,13 @@
 ﻿param (
-  [string]$packageSuffix = "0"
+  [string]$packageSuffix = "0",
+  [bool]$isLocal = $false,
+  [string]$outputDirectory = "..\..\buildoutput"
 )
 
+if ($isLocal){
+  $packageSuffix = "dev" + [datetime]::UtcNow.Ticks.ToString()
+  Write-Host "Local build - setting package suffixes to $packageSuffix" -ForegroundColor Yellow
+}
 dotnet --version
 
 dotnet build Webjobs.sln -v q
@@ -20,7 +26,7 @@ $projects =
 
 foreach ($project in $projects)
 {
-  $cmd = "pack", "$project", "-o", "..\..\buildoutput", "--no-build"
+  $cmd = "pack", "$project", "-o", $outputDirectory, "--no-build"
   
   if ($packageSuffix -ne "0")
   {
