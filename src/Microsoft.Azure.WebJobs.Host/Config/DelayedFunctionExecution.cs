2dcc2225346dea223f49b79806f9fd7666224570
Connor McMahon <connorm333@gmail.com>
2018-07-19 15:35:57 -0700

    Add post execution action

--- src/Microsoft.Azure.WebJobs.Host/Config/DelayedFunctionExecution.cs
+++ src/Microsoft.Azure.WebJobs.Host/Config/DelayedFunctionExecution.cs
@@ -11,16 +11,20 @@ namespace Microsoft.Azure.WebJobs.Host.Config
     {
         private readonly ITriggeredFunctionExecutor _executor;
         private readonly TriggeredFunctionData _data;
+        private readonly Func<Task> _postExecuteAction;
 
-        public DelayedFunctionExecution(ITriggeredFunctionExecutor executor, TriggeredFunctionData data)
+        public DelayedFunctionExecution(ITriggeredFunctionExecutor executor, TriggeredFunctionData data, Func<Task> postExecuteAction)
         {
             _executor = executor;
             _data = data;
+            _postExecuteAction = postExecuteAction;
         }
 
         public async Task<FunctionResult> ExecuteAsync(CancellationToken token)
         {
-            return await _executor.TryExecuteAsync(_data, token);
+            FunctionResult result = await _executor.TryExecuteAsync(_data, token);
+            await _postExecuteAction.Invoke();
+            return result;
         }
     }
 }
