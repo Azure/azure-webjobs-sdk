bca023ff851afb1301f6a45a632eef5a027de549
Cijo Thomas <cithomas@microsoft.com>
2019-04-24 11:23:39 -0700

    Added PerfCounterSDKVersionTelemetryInitializer

---
+++ src/Microsoft.Azure.WebJobs.Logging.ApplicationInsights/Initializers/PerfCounterSdkVersionTelemetryInitializer.cs
@@ -0,0 +1,30 @@
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the MIT License. See License.txt in the project root for license information.
+
+using Microsoft.ApplicationInsights.Channel;
+using Microsoft.ApplicationInsights.DataContracts;
+using Microsoft.ApplicationInsights.Extensibility;
+using Microsoft.ApplicationInsights.Extensibility.Implementation;
+
+namespace Microsoft.Azure.WebJobs.Logging.ApplicationInsights
+{
+    internal class PerfCounterSdkVersionTelemetryInitializer : ITelemetryInitializer
+    {
+        public void Initialize(ITelemetry telemetry)
+        {
+            if (telemetry == null)
+            {
+                return;
+            }
+            
+            if (telemetry is PerformanceCounterTelemetry)
+            {
+                var internalContext = telemetry.Context != null ? telemetry.Context.GetInternalContext() : null;
+                if (internalContext != null && internalContext.SdkVersion != null)
+                {
+                    internalContext.SdkVersion = "f_" + telemetry.Context.GetInternalContext().SdkVersion;
+                }
+            }
+        }
+    }
+}
