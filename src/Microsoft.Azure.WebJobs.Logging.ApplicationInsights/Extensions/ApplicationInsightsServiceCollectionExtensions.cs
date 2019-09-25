96f164da6c98e232a26f039bb0881cfc77e49d90
Liudmila Molkova <lmolkova@microsoft.com>
2019-09-24 19:00:48 -0700

    Fix W3C off

--- src/Microsoft.Azure.WebJobs.Logging.ApplicationInsights/Extensions/ApplicationInsightsServiceCollectionExtensions.cs
+++ src/Microsoft.Azure.WebJobs.Logging.ApplicationInsights/Extensions/ApplicationInsightsServiceCollectionExtensions.cs
@@ -139,6 +139,7 @@ namespace Microsoft.Extensions.DependencyInjection
                 Activity.DefaultIdFormat = options.HttpAutoCollectionOptions.EnableW3CDistributedTracing
                     ? ActivityIdFormat.W3C
                     : ActivityIdFormat.Hierarchical;
+                Activity.ForceDefaultIdFormat = true;
 
                 LoggerFilterOptions filterOptions = CreateFilterOptions(provider.GetService<IOptions<LoggerFilterOptions>>().Value);
 
