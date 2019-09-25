1e8479d748e0b13e5d957ad30447710ba0e3d7a3
Mathew Charles <mathewc@microsoft.com>
2019-09-25 09:38:07 -0700

    Removing InstanceId from metrics class

--- src/Microsoft.Azure.WebJobs.Host/Scale/ScaleMetrics.cs
+++ src/Microsoft.Azure.WebJobs.Host/Scale/ScaleMetrics.cs
@@ -16,14 +16,6 @@ namespace Microsoft.Azure.WebJobs.Host.Scale
         }
 
         /// <summary>
-        /// Gets or sets the worker instance this sample was taken by.
-        /// </summary>
-        /// <remarks>
-        /// This value will be set automatically by the system.
-        /// </remarks>
-        public string InstanceId { get; set; }
-
-        /// <summary>
         /// Gets or sets the UTC timestamp of when the sample was taken.
         /// </summary>
         public DateTime Timestamp { get; set; }
