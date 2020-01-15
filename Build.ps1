b3e29782dd5a5a01b644936b12089a5df29537c3
Naveen Kumar <v-naveen.kumar@blackbaud.com>
2020-01-15 14:52:34 +0530

    make FakeAzureStorage packable
    This change is to make FakeAzureStorage packable

--- Build.ps1
+++ Build.ps1
@@ -22,7 +22,8 @@ $projects =
   "src\Microsoft.Azure.WebJobs.Logging.ApplicationInsights\WebJobs.Logging.ApplicationInsights.csproj",
   "src\Microsoft.Azure.WebJobs.Extensions.Storage\WebJobs.Extensions.Storage.csproj",
   "src\Microsoft.Azure.WebJobs.Host.Storage\WebJobs.Host.Storage.csproj",
-  "test\Microsoft.Azure.WebJobs.Host.TestCommon\WebJobs.Host.TestCommon.csproj"
+  "test\Microsoft.Azure.WebJobs.Host.TestCommon\WebJobs.Host.TestCommon.csproj",
+  "test\FakeStorage\FakeAzureStorage.csproj"
 
 foreach ($project in $projects)
 {
