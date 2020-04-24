f7ab936b0a2edee4a4e3b87dd59512ff18199a69
Brett Samblanet <brettsam@microsoft.com>
2020-04-24 12:49:23 -0700

    [Storage] cancelling potential deadlock operations

--- src/Microsoft.Azure.WebJobs.Extensions.Storage/TimeoutHandler.cs
+++ src/Microsoft.Azure.WebJobs.Extensions.Storage/TimeoutHandler.cs
@@ -6,44 +6,76 @@ using System.Runtime.ExceptionServices;
 using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.Azure.WebJobs.Host.Timers;
+using Microsoft.Extensions.Logging;
 
 namespace Microsoft.Azure.WebJobs.Extensions.Storage
 {
     internal static class TimeoutHandler
     {
-        private static readonly TimeSpan DefaultTimeout = TimeSpan.FromMinutes(2);
+        private static readonly TimeSpan OperationTimeout = TimeSpan.FromMinutes(2);
+        private static readonly TimeSpan DeadlockTimeout = TimeSpan.FromMinutes(3);
+        private static readonly string Version = typeof(TimeoutHandler).GetType().Assembly.GetName().Version.ToString();
 
-        public static async Task<T> ExecuteWithTimeout<T>(string operationName, string clientRequestId, IWebJobsExceptionHandler exceptionHandler, Func<Task<T>> operation)
+        public static async Task<T> ExecuteWithTimeout<T>(string operationName, string clientRequestId, IWebJobsExceptionHandler exceptionHandler, ILogger logger, Func<CancellationToken, Task<T>> operation)
         {
-            using (var cts = new CancellationTokenSource())
+            using (var operationCts = new CancellationTokenSource())
             {
-                Task timeoutTask = Task.Delay(DefaultTimeout, cts.Token);
-                Task<T> operationTask = operation();
-
-                Task completedTask = await Task.WhenAny(timeoutTask, operationTask);
-
-                if (Equals(timeoutTask, completedTask))
+                using (var deadlockCts = new CancellationTokenSource())
                 {
-                    ExceptionDispatchInfo exceptionDispatchInfo;
+                    operationCts.CancelAfter(OperationTimeout);
+
                     try
                     {
-                        throw new TimeoutException($"The operation '{operationName}' with id '{clientRequestId}' did not complete in '{DefaultTimeout}'.");
+                        Task timeoutTask = Task.Delay(DeadlockTimeout, deadlockCts.Token);
+                        Task<T> operationTask = operation(operationCts.Token);
+
+                        Task completedTask = await Task.WhenAny(timeoutTask, operationTask);
+
+                        if (Equals(timeoutTask, completedTask))
+                        {
+                            // Just in case the operation cancellation doesn't work, cancel the whole thing and restart.
+                            await HandleDeadlockExceptionAsync(operationName, clientRequestId, exceptionHandler);
+
+                            return default;
+                        }
+
+                        return await operationTask;
                     }
-                    catch (TimeoutException ex)
+                    catch (OperationCanceledException)
                     {
-                        exceptionDispatchInfo = ExceptionDispatchInfo.Capture(ex);
+                        // Make sure it was this token that was canceled.
+                        if (operationCts.IsCancellationRequested)
+                        {
+                            logger.LogDebug($"The operation '{operationName}' with id '{clientRequestId}' was canceled after '{OperationTimeout}'. Version: '{Version}'");
+                        }
+                        else
+                        {
+                            throw;
+                        }
                     }
-
-                    await exceptionHandler.OnUnhandledExceptionAsync(exceptionDispatchInfo);
-
-                    return default(T);
+                    finally
+                    {
+                        deadlockCts.Cancel();
+                    }
                 }
-
-                // Cancel the Delay.
-                cts.Cancel();
-
-                return await operationTask;
             }
+
+            return default;
+        }
+
+        private static Task HandleDeadlockExceptionAsync(string operationName, string clientRequestId, IWebJobsExceptionHandler exceptionHandler)
+        {
+            ExceptionDispatchInfo exceptionDispatchInfo;
+            try
+            {
+                throw new TimeoutException($"The operation '{operationName}' with id '{clientRequestId}' did not complete in '{DeadlockTimeout}'. Version: '{Version}'");
+            }
+            catch (TimeoutException ex)
+            {
+                exceptionDispatchInfo = ExceptionDispatchInfo.Capture(ex);
+            }
+
+            return exceptionHandler.OnUnhandledExceptionAsync(exceptionDispatchInfo);
         }
     }
 }
