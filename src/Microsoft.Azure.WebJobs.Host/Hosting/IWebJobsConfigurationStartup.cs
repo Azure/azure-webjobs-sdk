0620ab95025e5e950f993a1f5a75bbf54e99b5ff
Brett Samblanet <brettsam@microsoft.com>
2020-05-13 06:29:05 -0700

    adding comments to IWebJobsConfigurationStartup

--- src/Microsoft.Azure.WebJobs.Host/Hosting/IWebJobsConfigurationStartup.cs
+++ src/Microsoft.Azure.WebJobs.Host/Hosting/IWebJobsConfigurationStartup.cs
@@ -3,8 +3,20 @@
 
 namespace Microsoft.Azure.WebJobs.Hosting
 {
+    /// <summary>
+    /// Interface defining a startup action for configuring application configuration
+    /// as part of host startup.
+    /// </summary>
     public interface IWebJobsConfigurationStartup
     {
+        /// <summary>
+        /// Performs the startup action for configuring application configuration with an 
+        /// <see cref="Extensions.Configuration.IConfigurationBuilder"></see>. The host will call this method at the
+        /// right time during host initialization.
+        /// </summary>
+        /// <param name="context">The builder context</param>
+        /// <param name="builder">The <see cref="IWebJobsConfigurationBuilder"/> that can be used to
+        /// configure the host application configuration.</param>
         void Configure(WebJobsBuilderContext context, IWebJobsConfigurationBuilder builder);
     }
 }
