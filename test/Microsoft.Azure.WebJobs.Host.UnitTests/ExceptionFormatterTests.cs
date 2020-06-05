815d44e47ed9d2d343d2357a26ea16ba36c2b0ef
Szymon Kulec <scooletz@gmail.com>
2020-06-05 18:32:34 +0200

    dummy full text assertions

--- test/Microsoft.Azure.WebJobs.Host.UnitTests/ExceptionFormatterTests.cs
+++ test/Microsoft.Azure.WebJobs.Host.UnitTests/ExceptionFormatterTests.cs
@@ -34,6 +34,9 @@ namespace Microsoft.Azure.WebJobs.Host.UnitTests
             catch (Exception exc)
             {
                 string formattedException = GetFormattedException(exc);
+
+                Assert.Equal(OutputRemovesAsyncFrames, formattedException);
+
                 Assert.DoesNotMatch("d__.\\.MoveNext()", formattedException);
                 Assert.DoesNotContain("TaskAwaiter", formattedException);
             }
@@ -50,6 +53,8 @@ namespace Microsoft.Azure.WebJobs.Host.UnitTests
             catch (Exception exc)
             {
                 string formattedException = GetFormattedException(exc);
+
+                Assert.Equal(OutputResolvesAsyncMethodNames, formattedException);
 
                 string typeName = $"{typeof(TestClass).DeclaringType.FullName}.{ nameof(TestClass)}";
                 Assert.Contains($"async {typeName}.{nameof(TestClass.Run1Async)}()", formattedException);
@@ -69,7 +74,10 @@ namespace Microsoft.Azure.WebJobs.Host.UnitTests
             catch (Exception exc)
             {
                 string formattedException = GetFormattedException(exc);
-                
+
+                Assert.Equal(OutputOutputsMethodParameters, formattedException);
+
+
                 Assert.Contains($"{nameof(TestClass.Run)}(String arg)", formattedException);
             }
         }
@@ -85,6 +93,8 @@ namespace Microsoft.Azure.WebJobs.Host.UnitTests
             catch (Exception exc)
             {
                 string formattedException = GetFormattedException(exc);
+
+                Assert.Equal(OutputOutputsExpectedAsyncMethodParameters, formattedException);
 
                 Assert.Contains($"{nameof(TestClass.Run4Async)}(String arg)", formattedException);
 
@@ -187,5 +197,66 @@ namespace Microsoft.Azure.WebJobs.Host.UnitTests
                 await CrashAsync();
             }
         }
+
+        const string OutputRemovesAsyncFrames =
+@"System.AggregateException : One or more errors occurred. (Crash!) ---> Crash!
+   at System.Threading.Tasks.Task.ThrowIfExceptional(Boolean includeTaskCanceledExceptions)
+   at System.Threading.Tasks.Task.Wait(Int32 millisecondsTimeout,CancellationToken cancellationToken)
+   at System.Threading.Tasks.Task.Wait()
+   at Microsoft.Azure.WebJobs.Host.UnitTests.ExceptionFormatterTests.TestClass.Run(String arg) at C:\GIT\azure-webjobs-sdk\test\Microsoft.Azure.WebJobs.Host.UnitTests\ExceptionFormatterTests.cs : 137
+   at Microsoft.Azure.WebJobs.Host.UnitTests.ExceptionFormatterTests.TestClass.Run() at C:\GIT\azure-webjobs-sdk\test\Microsoft.Azure.WebJobs.Host.UnitTests\ExceptionFormatterTests.cs : 129
+   at Microsoft.Azure.WebJobs.Host.UnitTests.ExceptionFormatterTests.FormatException_RemovesAsyncFrames() at C:\GIT\azure-webjobs-sdk\test\Microsoft.Azure.WebJobs.Host.UnitTests\ExceptionFormatterTests.cs : 32
+---> (Inner Exception #0) System.Exception : Crash!
+   at async Microsoft.Azure.WebJobs.Host.UnitTests.ExceptionFormatterTests.TestClass.CrashAsync() at C:\GIT\azure-webjobs-sdk\test\Microsoft.Azure.WebJobs.Host.UnitTests\ExceptionFormatterTests.cs : 179
+   at System.Runtime.ExceptionServices.ExceptionDispatchInfo.Throw()
+   at async Microsoft.Azure.WebJobs.Host.UnitTests.ExceptionFormatterTests.TestClass.Run2Async() at C:\GIT\azure-webjobs-sdk\test\Microsoft.Azure.WebJobs.Host.UnitTests\ExceptionFormatterTests.cs : 172
+   at System.Runtime.ExceptionServices.ExceptionDispatchInfo.Throw()
+   at async Microsoft.Azure.WebJobs.Host.UnitTests.ExceptionFormatterTests.TestClass.Run1Async() at C:\GIT\azure-webjobs-sdk\test\Microsoft.Azure.WebJobs.Host.UnitTests\ExceptionFormatterTests.cs : 166<---
+";
+
+        const string OutputResolvesAsyncMethodNames =
+@"System.AggregateException : One or more errors occurred. (Crash!) ---> Crash!
+   at System.Threading.Tasks.Task.ThrowIfExceptional(Boolean includeTaskCanceledExceptions)
+   at System.Threading.Tasks.Task.Wait(Int32 millisecondsTimeout,CancellationToken cancellationToken)
+   at System.Threading.Tasks.Task.Wait()
+   at Microsoft.Azure.WebJobs.Host.UnitTests.ExceptionFormatterTests.TestClass.Run(String arg) at C:\GIT\azure-webjobs-sdk\test\Microsoft.Azure.WebJobs.Host.UnitTests\ExceptionFormatterTests.cs : 137
+   at Microsoft.Azure.WebJobs.Host.UnitTests.ExceptionFormatterTests.TestClass.Run() at C:\GIT\azure-webjobs-sdk\test\Microsoft.Azure.WebJobs.Host.UnitTests\ExceptionFormatterTests.cs : 129
+   at Microsoft.Azure.WebJobs.Host.UnitTests.ExceptionFormatterTests.FormatException_ResolvesAsyncMethodNames() at C:\GIT\azure-webjobs-sdk\test\Microsoft.Azure.WebJobs.Host.UnitTests\ExceptionFormatterTests.cs : 51
+---> (Inner Exception #0) System.Exception : Crash!
+   at async Microsoft.Azure.WebJobs.Host.UnitTests.ExceptionFormatterTests.TestClass.CrashAsync() at C:\GIT\azure-webjobs-sdk\test\Microsoft.Azure.WebJobs.Host.UnitTests\ExceptionFormatterTests.cs : 179
+   at System.Runtime.ExceptionServices.ExceptionDispatchInfo.Throw()
+   at async Microsoft.Azure.WebJobs.Host.UnitTests.ExceptionFormatterTests.TestClass.Run2Async() at C:\GIT\azure-webjobs-sdk\test\Microsoft.Azure.WebJobs.Host.UnitTests\ExceptionFormatterTests.cs : 172
+   at System.Runtime.ExceptionServices.ExceptionDispatchInfo.Throw()
+   at async Microsoft.Azure.WebJobs.Host.UnitTests.ExceptionFormatterTests.TestClass.Run1Async() at C:\GIT\azure-webjobs-sdk\test\Microsoft.Azure.WebJobs.Host.UnitTests\ExceptionFormatterTests.cs : 166<---
+"; 
+        const string OutputOutputsMethodParameters =
+@"System.AggregateException : One or more errors occurred. (Crash!) ---> Crash!
+   at System.Threading.Tasks.Task.ThrowIfExceptional(Boolean includeTaskCanceledExceptions)
+   at System.Threading.Tasks.Task.Wait(Int32 millisecondsTimeout,CancellationToken cancellationToken)
+   at System.Threading.Tasks.Task.Wait()
+   at Microsoft.Azure.WebJobs.Host.UnitTests.ExceptionFormatterTests.TestClass.Run(String arg) at C:\GIT\azure-webjobs-sdk\test\Microsoft.Azure.WebJobs.Host.UnitTests\ExceptionFormatterTests.cs : 137
+   at Microsoft.Azure.WebJobs.Host.UnitTests.ExceptionFormatterTests.TestClass.Run() at C:\GIT\azure-webjobs-sdk\test\Microsoft.Azure.WebJobs.Host.UnitTests\ExceptionFormatterTests.cs : 129
+   at Microsoft.Azure.WebJobs.Host.UnitTests.ExceptionFormatterTests.FormatException_OutputsMethodParameters() at C:\GIT\azure-webjobs-sdk\test\Microsoft.Azure.WebJobs.Host.UnitTests\ExceptionFormatterTests.cs : 72
+---> (Inner Exception #0) System.Exception : Crash!
+   at async Microsoft.Azure.WebJobs.Host.UnitTests.ExceptionFormatterTests.TestClass.CrashAsync() at C:\GIT\azure-webjobs-sdk\test\Microsoft.Azure.WebJobs.Host.UnitTests\ExceptionFormatterTests.cs : 179
+   at System.Runtime.ExceptionServices.ExceptionDispatchInfo.Throw()
+   at async Microsoft.Azure.WebJobs.Host.UnitTests.ExceptionFormatterTests.TestClass.Run2Async() at C:\GIT\azure-webjobs-sdk\test\Microsoft.Azure.WebJobs.Host.UnitTests\ExceptionFormatterTests.cs : 172
+   at System.Runtime.ExceptionServices.ExceptionDispatchInfo.Throw()
+   at async Microsoft.Azure.WebJobs.Host.UnitTests.ExceptionFormatterTests.TestClass.Run1Async() at C:\GIT\azure-webjobs-sdk\test\Microsoft.Azure.WebJobs.Host.UnitTests\ExceptionFormatterTests.cs : 166<---
+";
+        const string OutputOutputsExpectedAsyncMethodParameters =
+@"System.AggregateException : One or more errors occurred. (Crash!) ---> Crash!
+   at System.Threading.Tasks.Task.ThrowIfExceptional(Boolean includeTaskCanceledExceptions)
+   at System.Threading.Tasks.Task.Wait(Int32 millisecondsTimeout,CancellationToken cancellationToken)
+   at System.Threading.Tasks.Task.Wait()
+   at Microsoft.Azure.WebJobs.Host.UnitTests.ExceptionFormatterTests.TestClass.Run(String arg) at C:\GIT\azure-webjobs-sdk\test\Microsoft.Azure.WebJobs.Host.UnitTests\ExceptionFormatterTests.cs : 141
+   at Microsoft.Azure.WebJobs.Host.UnitTests.ExceptionFormatterTests.FormatException_OutputsExpectedAsyncMethodParameters() at C:\GIT\azure-webjobs-sdk\test\Microsoft.Azure.WebJobs.Host.UnitTests\ExceptionFormatterTests.cs : 91
+---> (Inner Exception #0) System.Exception : Crash!
+   at async Microsoft.Azure.WebJobs.Host.UnitTests.ExceptionFormatterTests.TestClass.CrashAsync() at C:\GIT\azure-webjobs-sdk\test\Microsoft.Azure.WebJobs.Host.UnitTests\ExceptionFormatterTests.cs : 179
+   at System.Runtime.ExceptionServices.ExceptionDispatchInfo.Throw()
+   at async Microsoft.Azure.WebJobs.Host.UnitTests.ExceptionFormatterTests.TestClass.Run5Async(??) at C:\GIT\azure-webjobs-sdk\test\Microsoft.Azure.WebJobs.Host.UnitTests\ExceptionFormatterTests.cs : 191
+   at System.Runtime.ExceptionServices.ExceptionDispatchInfo.Throw()
+   at async Microsoft.Azure.WebJobs.Host.UnitTests.ExceptionFormatterTests.TestClass.Run4Async(String arg) at C:\GIT\azure-webjobs-sdk\test\Microsoft.Azure.WebJobs.Host.UnitTests\ExceptionFormatterTests.cs : 185<---
+";
     }
 }
