1ec5d268985e3849d59b4d0062365e8dc0357cb2
Szymon Kulec <scooletz@gmail.com>
2020-10-01 20:02:29 +0200

    benchmarks added

---
+++ test/Benchmarks/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using BenchmarkDotNet.Running;
+
+namespace Benchmarks
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            BenchmarkSwitcher.FromAssembly(typeof(Program).Assembly).Run(args);
+        }
+    }
+}
---
+++ test/Benchmarks/WebjobsExtensionsStorage.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using BenchmarkDotNet.Attributes;
+using Microsoft.Azure.Storage.Queue;
+using Microsoft.Azure.WebJobs.Host.Queues;
+using Newtonsoft.Json.Linq;
+
+namespace Benchmarks
+{
+    [MemoryDiagnoser]
+    public class WebjobsExtensionsStorage
+    {
+        readonly CloudQueueMessage _minimal;
+        readonly CloudQueueMessage _moderate;
+
+        public WebjobsExtensionsStorage()
+        {
+            var expected = Guid.NewGuid();
+            var minimal = new JObject
+            {
+                {"$AzureWebJobsParentId", new JValue(expected.ToString())}
+            };
+
+            _minimal = new CloudQueueMessage(minimal.ToString());
+
+            var moderate = new JObject
+            {
+                {"$AzureWebJobsParentId", new JValue(expected.ToString())},
+                {"BlobName", new JValue("some/path/to/blob")},
+                {"Date", new JValue(DateTime.Now)},
+                {"Number", new JValue(1234324)},
+            };
+
+            _moderate = new CloudQueueMessage(moderate.ToString());
+        }
+
+        [Benchmark]
+        public Guid? QueueCausalityManager_GetOwner_Minimal()
+        {
+            return QueueCausalityManager.GetOwner(_minimal);
+        }
+        
+        [Benchmark]
+        public Guid? QueueCausalityManager_GetOwner_Moderate()
+        {
+            return QueueCausalityManager.GetOwner(_moderate);
+        }
+    }
+}
