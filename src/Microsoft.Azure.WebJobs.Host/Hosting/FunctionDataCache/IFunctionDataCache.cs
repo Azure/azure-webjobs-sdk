cb46f8f87adce46ad3e75c69e4fd62a91111fe92
Gohar Irfan Chaudhry <gochaudh@microsoft.com>
2021-08-25 14:39:27 -0700

    Undo param rename

--- src/Microsoft.Azure.WebJobs.Host/Hosting/FunctionDataCache/IFunctionDataCache.cs
+++ src/Microsoft.Azure.WebJobs.Host/Hosting/FunctionDataCache/IFunctionDataCache.cs
@@ -21,9 +21,9 @@ namespace Microsoft.Azure.WebJobs
         /// <param name="cacheKey">Key corresponding to the object being inserted into the cache.</param>
         /// <param name="sharedMemoryMeta">Metadata about the shared memory region containing content of the object.</param>
         /// <param name="isIncrementActiveReference">If <see cref="true"/>, the reference counter for this object in the cache will be incremented (hence preventing it from being evicted).</param>
-        /// <param name="deleteOnFailure">If <see cref="true"/>, in the case where the cache is unable to insert this object, the shared memory region containing the content of the object is removed.</param>
+        /// <param name="isDeleteOnFailure">If <see cref="true"/>, in the case where the cache is unable to insert this object, the shared memory region containing the content of the object is removed.</param>
         /// <returns><see cref="true"/> if the object was added successfully, <see cref="false"/> otherwise.</returns>
-        bool TryPut(FunctionDataCacheKey cacheKey, SharedMemoryMetadata sharedMemoryMeta, bool isIncrementActiveReference, bool deleteOnFailure);
+        bool TryPut(FunctionDataCacheKey cacheKey, SharedMemoryMetadata sharedMemoryMeta, bool isIncrementActiveReference, bool isDeleteOnFailure);
 
         /// <summary>
         /// Get metadata about where the object corresponding to the given key is present.
