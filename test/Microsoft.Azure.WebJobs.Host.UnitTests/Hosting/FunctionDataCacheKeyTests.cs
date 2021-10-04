c4bcbf65e12b04234270a9e2616cd356682e2768
Kar Shin Lin <karslin@microsoft.com>
2021-10-04 15:44:31 -0700

    Replacing project refs with package refs for maintenance

--- test/Microsoft.Azure.WebJobs.Host.UnitTests/Hosting/FunctionDataCacheKeyTests.cs
+++ test/Microsoft.Azure.WebJobs.Host.UnitTests/Hosting/FunctionDataCacheKeyTests.cs
@@ -2,6 +2,7 @@
 // Licensed under the MIT License. See License.txt in the project root for license information.
 
 using System.Collections.Generic;
+using Microsoft.Azure.WebJobs;
 using Xunit;
 
 namespace Microsoft.Azure.WebJobs.Host.UnitTests.Hosting
