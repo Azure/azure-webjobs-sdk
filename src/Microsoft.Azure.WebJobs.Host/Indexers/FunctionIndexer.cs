953268ae6ebec9f4f0e6575f457729d17b8994a0
Alexey Rodionov <alrod@microsoft.com>
2021-12-02 17:40:19 -0800

    Fix comments

--- src/Microsoft.Azure.WebJobs.Host/Indexers/FunctionIndexer.cs
+++ src/Microsoft.Azure.WebJobs.Host/Indexers/FunctionIndexer.cs
@@ -460,11 +460,11 @@ namespace Microsoft.Azure.WebJobs.Host.Indexers
         {
             if (descr.RetryStrategy != null)
             {
-                string functionName = descr?.ShortName;
+                string functionName = descr.ShortName;
                 string triggerBindingName = triggerBinding?.GetType().Name;
-                string strategy = JsonConvert.SerializeObject(descr?.RetryStrategy);
+                string strategy = JsonConvert.SerializeObject(descr.RetryStrategy);
 
-                logger?.LogDebug("Retry strategy is defined for function '{FunctionName}' with trigger binding type 'TriggerBindingName'. Strategy = '{Strategy}'", 
+                logger?.LogDebug("Retry strategy is defined for function '{FunctionName}' with trigger binding type '{TriggerBindingName}'. Strategy = '{Strategy}'", 
                     functionName, triggerBindingName, strategy);
 
                 //Temporary comment: https://github.com/Azure/azure-webjobs-sdk/issues/2788
