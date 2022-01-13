59ddfced20b4e27820c597f841907a594ea227ed
Benjamin Hugot <benjmain.hugot@younited-credit.com>
2022-01-13 23:31:55 +0100

    another attempt

--- build.cmd
+++ build.cmd
@@ -1,2 +1,3 @@
 @ECHO OFF
+PowerShell -NoProfile -NoLogo -ExecutionPolicy unrestricted -Command "./dotnet-install.ps1 -Version 3.1.416 -Architecture x86 %*; exit $LASTEXITCODE"
 PowerShell -NoProfile -NoLogo -ExecutionPolicy unrestricted -Command "./build.ps1 %*; exit $LASTEXITCODE"
