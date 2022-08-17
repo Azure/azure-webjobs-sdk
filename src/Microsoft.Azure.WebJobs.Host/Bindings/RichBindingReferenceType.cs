88b003433f4fac732d0b1abd8d22f754979c3707
Surgupta <surgupta@microsoft.com>
2022-08-17 10:28:42 -0700

    Added RichBindingReferenceType

---
+++ src/Microsoft.Azure.WebJobs.Host/Bindings/RichBindingReferenceType.cs
@@ -0,0 +1,21 @@
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the MIT License. See License.txt in the project root for license information.
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Xml.Linq;
+
+namespace Microsoft.Azure.WebJobs.Script.Abstractions.Description.Binding
+{
+    public class RichBindingReferenceType
+    {
+        /// <summary>
+        /// Gets or Sets all the properties of ReferenceType
+        /// The storage account connection string
+        /// The name of the container where the blob exists
+        /// The name of the blob
+        /// </summary>
+        public IDictionary<string, string> Properties { get; set; }
+    }
+}
