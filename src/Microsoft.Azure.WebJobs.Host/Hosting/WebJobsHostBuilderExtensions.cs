33a8ab9aa19ede09630b898142888d694c22930a
Mathew Charles <mathewc@microsoft.com>
2022-08-17 11:52:29 -0700

    Adding option for ConfigureWebJobs to skip adding default configuration sources.

--- src/Microsoft.Azure.WebJobs.Host/Hosting/WebJobsHostBuilderExtensions.cs
+++ src/Microsoft.Azure.WebJobs.Host/Hosting/WebJobsHostBuilderExtensions.cs
@@ -12,38 +12,94 @@ namespace Microsoft.Extensions.Hosting
 {
     public static class WebJobsHostBuilderExtensions
     {
-        public static IHostBuilder ConfigureWebJobs(this IHostBuilder builder)
+        /// <summary>
+        /// Configure WebJobs support for the host.
+        /// </summary>
+        /// <param name="builder">The <see cref="IHostBuilder"/> to configure.</param>
+        /// <param name="withDefaultAppConfiguration">Indicates whether default configuration sources should be added. The default is true.
+        /// If you're calling <see cref="IHostBuilder.ConfigureAppConfiguration(Action{HostBuilderContext, IConfigurationBuilder})"/> and
+        /// want to fully control configuration, specify false.</param>
+        /// <returns></returns>
+        public static IHostBuilder ConfigureWebJobs(this IHostBuilder builder, bool withDefaultAppConfiguration = true)
         {
-            return builder.ConfigureWebJobs(o => { }, o => { });
+            return builder.ConfigureWebJobs(o => { }, o => { }, withDefaultAppConfiguration);
         }
 
-        public static IHostBuilder ConfigureWebJobs(this IHostBuilder builder, Action<IWebJobsBuilder> configure)
+        /// <summary>
+        /// Configure WebJobs support for the host.
+        /// </summary>
+        /// <param name="builder">The <see cref="IHostBuilder"/> to configure.</param>
+        /// <param name="configure">Action to perform WebJobs configuration.</param>
+        /// <param name="withDefaultAppConfiguration">Indicates whether default configuration sources should be added. The default is true.
+        /// If you're calling <see cref="IHostBuilder.ConfigureAppConfiguration(Action{HostBuilderContext, IConfigurationBuilder})"/> and
+        /// want to fully control configuration, specify false.</param>
+        /// <returns></returns>
+        public static IHostBuilder ConfigureWebJobs(this IHostBuilder builder, Action<IWebJobsBuilder> configure, bool withDefaultAppConfiguration = true)
         {
-            return builder.ConfigureWebJobs(configure, o => { });
+            return builder.ConfigureWebJobs(configure, o => { }, withDefaultAppConfiguration);
         }
 
-        public static IHostBuilder ConfigureWebJobs(this IHostBuilder builder, Action<IWebJobsBuilder> configure, Action<JobHostOptions> configureOptions)
+        /// <summary>
+        /// Configure WebJobs support for the host.
+        /// </summary>
+        /// <param name="builder">The <see cref="IHostBuilder"/> to configure.</param>
+        /// <param name="configure">Action to perform WebJobs configuration.</param>
+        /// <param name="configureOptions">Action to configure <see cref="JobHostOptions"/>.</param>
+        /// <param name="withDefaultAppConfiguration">Indicates whether default configuration sources should be added. The default is true.
+        /// If you're calling <see cref="IHostBuilder.ConfigureAppConfiguration(Action{HostBuilderContext, IConfigurationBuilder})"/> and
+        /// want to fully control configuration, specify false.</param>
+        /// <returns></returns>
+        public static IHostBuilder ConfigureWebJobs(this IHostBuilder builder, Action<IWebJobsBuilder> configure, Action<JobHostOptions> configureOptions, bool withDefaultAppConfiguration = true)
         {
-            return builder.ConfigureWebJobs((context, b) => configure(b), configureOptions);
+            return builder.ConfigureWebJobs((context, b) => configure(b), configureOptions, withDefaultAppConfiguration);
         }
 
-        public static IHostBuilder ConfigureWebJobs(this IHostBuilder builder, Action<HostBuilderContext, IWebJobsBuilder> configure)
+        /// <summary>
+        /// Configure WebJobs support for the host.
+        /// </summary>
+        /// <param name="builder">The <see cref="IHostBuilder"/> to configure.</param>
+        /// <param name="configure">Action to perform WebJobs configuration.</param>
+        /// <param name="withDefaultAppConfiguration">Indicates whether default configuration sources should be added. The default is true.
+        /// If you're calling <see cref="IHostBuilder.ConfigureAppConfiguration(Action{HostBuilderContext, IConfigurationBuilder})"/> and
+        /// want to fully control configuration, specify false.</param>
+        /// <returns></returns>
+        public static IHostBuilder ConfigureWebJobs(this IHostBuilder builder, Action<HostBuilderContext, IWebJobsBuilder> configure, bool withDefaultAppConfiguration = true)
         {
-            return builder.ConfigureWebJobs(configure, o => { });
+            return builder.ConfigureWebJobs(configure, o => { }, withDefaultAppConfiguration);
         }
 
-        public static IHostBuilder ConfigureWebJobs(this IHostBuilder builder, Action<HostBuilderContext, IWebJobsBuilder> configure, Action<JobHostOptions> configureOptions)
+        /// <summary>
+        /// Configure WebJobs support for the host.
+        /// </summary>
+        /// <param name="builder"></param>
+        /// <param name="configure"></param>
+        /// <param name="configureOptions"></param>
+        /// <param name="withDefaultAppConfiguration"></param>
+        /// <returns></returns>
+        public static IHostBuilder ConfigureWebJobs(this IHostBuilder builder, Action<HostBuilderContext, IWebJobsBuilder> configure, Action<JobHostOptions> configureOptions, bool withDefaultAppConfiguration = true)
         {
-            return ConfigureWebJobs(builder, configure, configureOptions, (c, b) => { });
+            return ConfigureWebJobs(builder, configure, configureOptions, (c, b) => { }, withDefaultAppConfiguration);
         }
 
+        /// <summary>
+        /// Configure WebJobs support for the host.
+        /// </summary>
+        /// <param name="builder"></param>
+        /// <param name="configure"></param>
+        /// <param name="configureOptions"></param>
+        /// <param name="configureAppConfiguration"></param>
+        /// <param name="withDefaultAppConfiguration"></param>
+        /// <returns></returns>
         public static IHostBuilder ConfigureWebJobs(this IHostBuilder builder, Action<HostBuilderContext, IWebJobsBuilder> configure,
-            Action<JobHostOptions> configureOptions, Action<HostBuilderContext, IWebJobsConfigurationBuilder> configureAppConfiguration)
+            Action<JobHostOptions> configureOptions, Action<HostBuilderContext, IWebJobsConfigurationBuilder> configureAppConfiguration, bool withDefaultAppConfiguration = true)
         {
             builder.ConfigureAppConfiguration((context, config) =>
             {
-                config.AddJsonFile("appsettings.json", optional: true);
-                config.AddEnvironmentVariables();
+                if (withDefaultAppConfiguration)
+                {
+                    config.AddJsonFile("appsettings.json", optional: true);
+                    config.AddEnvironmentVariables();
+                }
 
                 configureAppConfiguration?.Invoke(context, new WebJobsConfigurationBuilder(config));
             });
