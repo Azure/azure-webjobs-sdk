4d35416d145e0d702a87af342881d3ad19b4790d
Lilian Kasem <likasem@microsoft.com>
2022-09-07 14:16:12 -0700

    Initilize prop dict

--- src/Microsoft.Azure.WebJobs.Host/Bindings/ParameterBindingData.cs
+++ src/Microsoft.Azure.WebJobs.Host/Bindings/ParameterBindingData.cs
@@ -1,21 +1,15 @@
 ﻿// Copyright (c) .NET Foundation. All rights reserved.
 // Licensed under the MIT License. See License.txt in the project root for license information.
 
-using System;
 using System.Collections.Generic;
-using System.Text;
-using System.Xml.Linq;
 
 namespace Microsoft.Azure.WebJobs.Script.Abstractions.Description.Binding
 {
     public class ParameterBindingData
     {
         /// <summary>
-        /// Gets or Sets all the properties of ReferenceType
-        /// The storage account connection string
-        /// The name of the container where the blob exists
-        /// The name of the blob
+        /// Dictionary for the properties required to hydrate SDK-type objects.
         /// </summary>
-        public IDictionary<string, string> Properties { get; set; }
+        public IDictionary<string, string> Properties = new Dictionary<string, string>();
     }
 }
