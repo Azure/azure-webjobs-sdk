07f2a5a0230cc3ba8f8c61ce5c52bc98a9c6ca09
Alexey Rodionov <alrod@inbox.ru>
2022-11-08 10:50:16 -0800

    Adding FunctionId to ScaleMonitorDescriptor (#2932)

--- src/Microsoft.Azure.WebJobs.Host/Scale/ScaleMonitorDescriptor.cs
+++ src/Microsoft.Azure.WebJobs.Host/Scale/ScaleMonitorDescriptor.cs
@@ -1,6 +1,8 @@
 ﻿// Copyright (c) .NET Foundation. All rights reserved.
 // Licensed under the MIT License. See License.txt in the project root for license information.
 
+using System;
+
 namespace Microsoft.Azure.WebJobs.Host.Scale
 {
     /// <summary>
@@ -8,9 +10,15 @@ namespace Microsoft.Azure.WebJobs.Host.Scale
     /// </summary>
     public class ScaleMonitorDescriptor
     {
+        [Obsolete("This constructor is obsolete. Use the version that takes function id instead.")]
         public ScaleMonitorDescriptor(string id)
         {
             Id = id;
+        }
+
+        public ScaleMonitorDescriptor(string id, string functionId) : this(id)
+        {
+            FunctionId = functionId;
         }
 
         /// <summary>
@@ -22,5 +30,10 @@ namespace Microsoft.Azure.WebJobs.Host.Scale
         /// be of the form "{FunctionId}-QueueTrigger-{QueueName}".
         /// </remarks>
         public string Id { get; }
+
+        /// <summary>
+        /// Gets the ID of the function associated with this monitor.
+        /// </summary>
+        public string FunctionId { get; }
     }
 }
