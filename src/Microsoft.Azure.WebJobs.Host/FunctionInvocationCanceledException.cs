eebeffa9bf96cc466ad6ffbccfe505fb6776cf5b
sarah <35204912+satvu@users.noreply.github.com>
2025-08-06 13:45:03 -0700

    Add FunctionAbortedException and FunctionCanceledException (#3139)
    * add functionAbortedException
    
    * respond to comments, add function invocation canceled exception
    
    * update public surface test
    
    * make canceled exception public
    
    * update log to remove worker ref
    
    * update log nit
    
    * update naming
    
    * update naming again
    
    * move files

---
+++ src/Microsoft.Azure.WebJobs.Host/FunctionInvocationCanceledException.cs
@@ -0,0 +1,13 @@
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the MIT License. See License.txt in the project root for license information.
+
+using System;
+
+namespace Microsoft.Azure.WebJobs.Host
+{
+    public class FunctionInvocationCanceledException : FunctionInvocationException
+    {
+        public FunctionInvocationCanceledException(string invocationId, Exception innerException)
+            : base($"The invocation request with Id '{invocationId}' was canceled.", innerException) { }
+    }
+}
