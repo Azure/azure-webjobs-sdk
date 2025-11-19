b647026b4fd0c088b3e30054cb1a49efff18ff2c
Brett Samblanet <brettsam@microsoft.com>
2025-11-19 11:21:30 -0800

    null checks in Binder

--- src/Microsoft.Azure.WebJobs.Host/Bindings/Runtime/Binder.cs
+++ src/Microsoft.Azure.WebJobs.Host/Bindings/Runtime/Binder.cs
@@ -153,8 +153,18 @@ namespace Microsoft.Azure.WebJobs
         /// <returns></returns>
         internal async Task Complete(CancellationToken cancellationToken)
         {
+            if (_binders == null)
+            {
+                return;
+            }
+
             foreach (IValueBinder binder in _binders)
             {
+                if (binder == null)
+                {
+                    continue;
+                }
+
                 // Binding can only be uses for non-Out parameters, and their binders ignore this argument.
                 await binder.SetValueAsync(value: null, cancellationToken: cancellationToken);
             }
